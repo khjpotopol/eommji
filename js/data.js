95a4cb2c5d12c020f2dd9d5f9d49abe633ef2dca
khjpotopol <71070659+khjpotopol@users.noreply.github.com>
2023-01-18 17:30:51 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,304 @@
+const penList = [
+    {
+        src:"../images/goods/pen/01.jpg",
+        id:"pen01",
+        name : "워터펜블랙+완자문소가죽반지갑",
+        price:"25415"
+    },
+    {
+        src:"../images/goods/pen/02.jpg",
+        id:"pen02",
+        name : "까렌다쉬849메탈샤프",
+        price:"26450"
+    },
+    {
+        src:"../images/goods/pen/03.jpg",
+        id:"pen03",
+        name : "포인트UV니들터치볼펜",
+        price:"449"
+    },
+    {
+        src:"../images/goods/pen/04.jpg",
+        id:"pen04",
+        name : "기화펜심세트",
+        price:"1093"
+    },
+    {
+        src:"../images/goods/pen/05.jpg",
+        id:"pen05",
+        name : "M터치펜",
+        price:"575"
+    },
+    {
+        src:"../images/goods/pen/06.jpg",
+        id:"pen06",
+        name : "동아필승2컴퓨터싸인펜+스텐다드샤프세트",
+        price:"604"
+    },
+    {
+        src:"../images/goods/pen/07.jpg",
+        id:"pen07",
+        name : "몽블랑펜",
+        price:"2415"
+    },
+    {
+        src:"../images/goods/pen/08.jpg",
+        id:"pen08",
+        name : "BT21리틀버디샤프(0.5mm)",
+        price:"1840"
+    },
+    {
+        src:"../images/goods/pen/09.jpg",
+        id:"pen09",
+        name : "헬프맨더블랙메탈볼펜(케이스포함)",
+        price:"2070"
+    },
+    {
+        src:"../images/goods/pen/10.jpg",
+        id:"pen10",
+        name : "[카카오프렌즈] 리틀프렌즈 연필 &amp; 캡 세트(4+4)",
+        price:"2542"
+    },
+    {
+        src:"../images/goods/pen/11.jpg",
+        id:"pen11",
+        name : "아이스크림펜(1.0mm)",
+        price:"460"
+    },
+    {
+        src:"../images/goods/pen/12.jpg",
+        id:"pen12",
+        name : "플래티넘프레이져만년필",
+        price:"11845"
+    },
+    {
+        src:"../images/goods/pen/13.jpg",
+        id:"pen13",
+        name : "[루이스코단]타조가죽볼펜+수성펜SET",
+        price:"47150"
+    },
+    {
+        src:"../images/goods/pen/14.jpg",
+        id:"pen14",
+        name : "동아 네임펜1.0mm",
+        price:"207"
+    },
+    {
+        src:"../images/goods/pen/15.jpg",
+        id:"pen15",
+        name : "[국산]투명사각H무지개A연필3P세트",
+        price:"403"
+    },
+    {
+        src:"../images/goods/pen/16.jpg",
+        id:"pen16",
+        name : "[BIC] 빅 에콜루션 연필(2B)",
+        price:"242"
+    },
+    {
+        src:"../images/goods/pen/17.jpg",
+        id:"pen17",
+        name : "4색 책갈피 볼펜",
+        price:"212"
+    },
+    {
+        src:"../images/goods/pen/18.jpg",
+        id:"pen18",
+        name : "파스텔 슬림터치볼펜",
+        price:"315"
+    },
+    {
+        src:"../images/goods/pen/19.jpg",
+        id:"pen19",
+        name : "연파스텔 고무그립슬림볼펜",
+        price:"351"
+    },
+    {
+        src:"../images/goods/pen/20.jpg",
+        id:"pen20",
+        name : "국산 콩나물니들펜0.7mm 2color",
+        price:"633"
+    },
+    {
+        src:"../images/goods/pen/21.jpg",
+        id:"pen21",
+        name : "[컬러인쇄]유로3색 화이트3링볼펜(독일잉크)",
+        price:"224"
+    },
+    {
+        src:"../images/goods/pen/22.jpg",
+        id:"pen22",
+        name : "유로3색 화이트3링볼펜(독일잉크)",
+        price:"161"
+    },
+    {
+        src:"../images/goods/pen/91.jpg",
+        id:"pen23",
+        name : "룩소르알프스412",
+        price:"8050"
+    },
+    {
+        src:"../images/goods/pen/24.jpg",
+        id:"pen24",
+        name : "[루이스코단]로만(미니)볼펜",
+        price:"9430"
+    },
+    {
+        src:"../images/goods/pen/25.jpg",
+        id:"pen25",
+        name : "불투명2색볼펜+델타형광펜세트",
+        price:"529"
+    },
+    {
+        src:"../images/goods/pen/26.jpg",
+        id:"pen26",
+        name : "BT21 베이비 중성펜(0.38mm)",
+        price:"1173"
+    },
+    {
+        src:"../images/goods/pen/27.jpg",
+        id:"pen27",
+        name : "(국산)스타사각형광펜5p세트(불투명)",
+        price:"920"
+    },
+    {
+        src:"../images/goods/pen/28.jpg",
+        id:"pen28",
+        name : "모던오피스 아이디어 12섹 투턴 듀얼 형광펜 6p 세트",
+        price:"3105"
+    },
+    {
+        src:"../images/goods/pen/29.jpg",
+        id:"pen29",
+        name : "[국산]밀키펫샤프0.5mm",
+        price:"690"
+    },
+    {
+        src:"../images/goods/pen/30.jpg",
+        id:"pen30",
+        name : "[국산]멍이펜 11본세트(10color)",
+        price:"4600"
+    },
+    {
+        src:"../images/goods/pen/31.jpg",
+        id:"pen31",
+        name : "포에버 4색볼펜(3색+형광심)",
+        price:"863"
+    },
+    {
+        src:"../images/goods/pen/32.jpg",
+        id:"pen32",
+        name : "모던오피스 사각 파스텔 혈광펜 12색 세트 CB305",
+        price:"7705"
+    },
+    {
+        src:"../images/goods/pen/33.jpg",
+        id:"pen33",
+        name : "[BIC]빅 클릭골드 콤비 세트 (2P)",
+        price:"25415"
+    },
+    {
+        src:"../images/goods/pen/34.jpg",
+        id:"pen34",
+        name : "뉴원3색투명",
+        price:"207"
+    },
+    {
+        src:"../images/goods/pen/35.jpg",
+        id:"pen35",
+        name : "자바-제트라인3P세트",
+        price:"1691"
+    },
+    {
+        src:"../images/goods/pen/36.jpg",
+        id:"pen36",
+        name : "[제프라]사라사3플러스S",
+        price:"4646"
+    },
+    {
+        src:"../images/goods/pen/37.jpg",
+        id:"pen37",
+        name : "[제프라] 블렌",
+        price:"1162"
+    },
+    {
+        src:"../images/goods/pen/38.jpg",
+        id:"pen38",
+        name : "룩소르(국산)세도나 스마트사각네임펜3P세트알프스412",
+        price:"822"
+    },
+    {
+        src:"../images/goods/pen/39.jpg",
+        id:"pen39",
+        name : "동아 겔스트림0.7mm",
+        price:"575"
+    },
+    {
+        src:"../images/goods/pen/40.jpg",
+        id:"pen40",
+        name : "[슈나이더]썬라이트볼펜",
+        price:"564"
+    },
+    {
+        src:"../images/goods/pen/41.jpg",
+        id:"pen41",
+        name : "[루이스코단]타조가죽볼펜/수성펜",
+        price:"17020"
+    },
+    {
+        src:"../images/goods/pen/42.jpg",
+        id:"pen42",
+        name : "[루이스코단]마블브라운볼펜/수성펜",
+        price:"17250"
+    },
+    {
+        src:"../images/goods/pen/43.jpg",
+        id:"pen43",
+        name : "ALL UV 레인보우 3색 터치펜",
+        price:"518"
+    },
+    {
+        src:"../images/goods/pen/44.jpg",
+        id:"pen44",
+        name : "[주문제작]캐릭터 볼펜(3D-4색 볼펜)",
+        price:"1380"
+    },
+    {
+        src:"../images/goods/pen/45.jpg",
+        id:"pen45",
+        name : "샤넬볼펜",
+        price:"1380"
+    },
+    {
+        src:"../images/goods/pen/46.jpg",
+        id:"pen46",
+        name : "컬러명함케이스(블랙)+갤럭시터치펜 세트",
+        price:"4428"
+    },
+    {
+        src:"../images/goods/pen/47.jpg",
+        id:"pen47",
+        name : "동아오피스연필(BHB)",
+        price:"207"
+    },
+    {
+        src:"../images/goods/pen/48.jpg",
+        id:"pen48",
+        name : "룩소르스털링세트416)",
+        price:"9775"
+    },
+    {
+        src:"../images/goods/pen/49.jpg",
+        id:"pen49",
+        name : "BT21 리틀버디 4색 볼펜(3색볼펜+형광젤펜)",
+        price:"2668"
+    },
+    {
+        src:"../images/goods/pen/50.jpg",
+        id:"pen50",
+        name : "BT21 드림 연필캡 7P세트",
+        price:"1748"
+    }   
+]
+
+export default penList
---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+/*메인비주얼*/
+let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        observer:true,
+        observeParents:true,
+      });
+
+/*customized*/
+
